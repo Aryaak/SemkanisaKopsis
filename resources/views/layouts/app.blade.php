7e4c8dc319f40b40d45117c7815b1bbddab18129
aryaaak <aryarizky2303@gmail.com>
2021-11-28 18:11:27 +0700

    update view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
 
         <title>{{ config('app.name', 'Semkanisa Kopsis Dashboard') }}</title>
         <!-- Favicon -->
-        <link href="{{ asset('public/argon/img/brand/favicon.png') }}" rel="icon" type="image/png">
+        <link href="{{ 'public/argon/img/brand/favicon.png' }}" rel="icon" type="image/png">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
         <!-- Extra details for Live View on GitHub Pages -->
